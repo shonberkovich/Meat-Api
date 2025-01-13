8299415eb6ff63c7917a4c4da9cf5bb5fa23f9f0
shonb <shonberko@gmail.com>
2025-01-13 21:19:36 +0200

    first commit

---
+++ src/main/java/org/example/productapi1/ProductApi1Application.java
@@ -0,0 +1,13 @@
+package org.example.productapi1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApi1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApi1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/productapi1/ProductApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.productapi1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApi1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
